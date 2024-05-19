cf44a63358f403d86acb3df2dc8075fc8fc13e66
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-19 17:36:58 +0200

    task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+
+$(document).ready(() => {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
