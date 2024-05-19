58e06ed9f93875362f54a478832a132e131e3e97
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-19 19:21:48 +0200

    task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8192,
+                    name: '[name].[ext]',
+                    outputPath: 'images',
+                },
+            },
+        ],
+    },
+};
