650e17b0236d84b6e4ac194459f207bef7bd74fb
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-23 17:42:22 +0200

    task 5

---
+++ 0x01-react_intro/task_5/dashboard/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../assests/holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Holberton Logo */}
+        <img src={logo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        {/* Add your content here */}
+        <p>Login to access the full dashboard</p>
+        <label>Email:</label>
+        <input type='email' placeholder='example@gmail.com'></input>
+        <label>Password:</label>
+        <input type='password' placeholder='Enter Your Password'></input>
+        <button>OK</button>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright { getFullYear() } - { getFooterCopy(true) }</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
