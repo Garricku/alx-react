37571f8d728d8cacec03bbd851d14060decf9f8f
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-24 19:35:15 +0200

    task 3 begin base files added

---
+++ 0x02-react_props/task_2/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+import logo from '../assets/holberton-logo.jpg';
+
+export default function appHeader() {
+return (
+    <header className="App-header">
+        {/* Holberton Logo */}
+        <img src={logo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+    </header>
+    );
+}
