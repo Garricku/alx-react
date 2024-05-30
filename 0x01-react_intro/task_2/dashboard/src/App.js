d05f1b4da90c598a1d32fe2f26fe5f9c7e0717ce
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-30 20:34:00 +0200

    tests corrected fixed the issues with the testings not working due to enzyme not being properly installed

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -7,18 +7,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Holberton Logo */}
         <img src={logo} alt="Holberton Logo" />
         <h1>School dashboard</h1>
       </header>
       <div className="App-body">
-        {/* Add your content here */}
         <p>Login to access the full dashboard</p>
-        <label>Email:</label>
-        <input type='email' placeholder='example@gmail.com'></input>
-        <label>Password:</label>
-        <input type='password' placeholder='Enter Your Password'></input>
-        <button>OK</button>
+        <form>
+          <label htmlFor='email'>Email:
+            <input id='email' type='email' placeholder='example@gmail.com' autoComplete='on'></input>
+          </label>
+          <label htmlFor='password'>Password:
+            <input id='password' type='password' placeholder='Enter Your Password' autoComplete='on'></input>
+          </label>
+          <button>OK</button>
+        </form>
       </div>
       <footer className="App-footer">
         <p>Copyright { getFullYear() } - { getFooterCopy(true) }</p>
