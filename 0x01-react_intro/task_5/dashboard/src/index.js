b2aa4143d0752a27373da01f2e427f91052c83a4
Garrick Larson Potts <ricki.glp@gmail.com>
2024-05-31 18:29:00 +0200

    Not looking too good, i added the jquery code but its very bad to read like this. there are no errors but the display is not showing... its a hard life we live

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,7 +1,11 @@
 import $ from 'jquery';
 
-
 $(document).ready(() => {
-    $('body').append(Notifications);
-    $('body').append(App);
+    $('body').append('<div><button><img src={closeIcon} alt="Close" /></button></div>')
+    $('body').append('<p>Here is the list of notifications</p>')
+    $('body').append('<ul><li data-priority="default">New course available</li><li data-priority="urgent">New resume available</li><li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}/></ul></div>')
+    $('body').append('<div className="App"><header className="App-header"><img src={logo} alt="Holberton Logo" /><h1>School dashboard</h1></header></div>')
+    $('body').append('<div className="App-body"><p>Login to access the full dashboard</p>')
+    $('body').append('<div><form><label htmlFor="email">Email:<input id="email" type="email" placeholder="example@gmail.com" autoComplete="on"></input></label><label htmlFor="password">Password:<input id="password" type="password" placeholder="Enter Your Password" autoComplete="on"></input></label><button>OK</button></form></div>')
+    $('body').append('<footer className="App-footer"><p>Copyright { getFullYear() } - { getFooterCopy(true) }</p></footer></div>');
 });
