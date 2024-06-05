8b6b0470765c2874adbe1940004a22079153ba63
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-05 16:53:30 +0200

    new project base files and readme

---
+++ 0x05-react_state/README.md
@@ -0,0 +1,43 @@
+# React state
+### Front-end
+### JavaScript
+### ES6
+### React
+
+## Resources
+*Read or watch:*
+
+- State and lifecycle
+- SetState and State callback
+- Context
+- Forms and Controlled components
+- Lifting State Up
+- React Hooks
+- Enzyme State
+- Enzyme SetState
+- Enzyme Instance
+- Enzyme Simulate
+
+## Learning Objectives
+
+*At the end of this project, you are expected to be able to explain to anyone, without the help of Google:*
+
+- What the state of a component or a container is
+- The lifecycle of a component
+- How to modify a state and execute code in the right order
+- What a controlled component is
+- How to use Forms in React
+- How to reuse smaller components, keep them pure, and lift its state to principal containers
+- The use of a React Hook and how to create one
+- How to test State changes with Enzyme
+
+## Requirements
+
+- All your files will be interpreted/compiled on Ubuntu 18.04 LTS using node 12.x.x and npm 6.x.x
+- Allowed editors:
+    - vi
+    - vim
+    - emacs
+    - Visual Studio Code
+- All your files should end with a new line
+- A README.md file, at the root of the folder of the project, is mandatory
---
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite"; // Import Aphrodite
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  handleKeyPress(e) {
+    if (e.ctrlKey && e.key === "h") {
+      e.preventDefault();
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={css(styles.app)}> {/* Apply Aphrodite styles */}
+          <div className={css(styles.headingSection)}>
+            <Notifications listNotifications={this.listNotifications} />
+            <Header />
+          </div>
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the school">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
+              iste vero dolor voluptates.
+            </p>
+          </BodySection>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+// Define Aphrodite styles
+const styles = StyleSheet.create({
+  app: {
+    margin: "10px",
+    fontFamily: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+  },
+  headingSection: {
+    ".App h1": {
+      fontFamily: "'Raleway', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    },
+  },
+});
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {
+    return;
+  },
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+export default App;
