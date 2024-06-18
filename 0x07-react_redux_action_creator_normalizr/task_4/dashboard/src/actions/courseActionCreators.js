4978ea2f0b83577558c9fd613fdbee46ccd863da
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-18 16:46:00 +0200

    task 4 cp

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,9 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export function selectCourse(index) {
+  return { type: SELECT_COURSE, index };
+}
+
+export function unSelectCourse(index) {
+  return { type: UNSELECT_COURSE, index };
+}
