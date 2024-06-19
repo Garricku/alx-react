d64dbab95733f374c01c6e09ab9ea8bc3368396e
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-19 14:22:58 +0200

    task 7

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -1,4 +1,5 @@
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from './uiActionTypes';
+import fetch from 'node-fetch';
 
 export function login(email, password) {
   return dispatch => dispatch({ type: LOGIN, user: { email, password } });
@@ -14,4 +15,29 @@ export function displayNotificationDrawer() {
 
 export function hideNotificationDrawer() {
   return dispatch => dispatch({ type: HIDE_NOTIFICATION_DRAWER });
+}
+
+export function loginSuccess() {
+  return { type: LOGIN_SUCCESS };
+}
+
+export function loginFailure() {
+  return { type: LOGIN_FAILURE };
+}
+
+export function loginRequest(email, password) {
+  return (dispatch) => {
+    dispatch(login(email, password));
+    return fetch('../../dist/login-success.json')
+      .then(response => {
+        if (response.ok) {
+          dispatch(loginSuccess());
+        } else {
+          dispatch(loginFailure());
+        }
+      })
+      .catch(() => {
+        dispatch(loginFailure());
+      });
+  };
 }
